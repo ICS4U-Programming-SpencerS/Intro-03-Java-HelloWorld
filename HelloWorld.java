64f89fed36d971d785269183f7e6475a4c56596f
Spencer S <spencer.scarlet@stu.ocsb.ca>
2024-02-09 00:17:09 +0000

    Created HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+
+* Small Hello world program, designed to test style checkers.
+
+*
+
+* @author  Spencer Scarlett
+
+* @version 1.0
+
+* @since   2024-Feb-08
+
+*/
+
+// Main function for the program.
+public class HelloWorld {
+    public static void main(String[] args) {
+        // two awesome print statements
+        System.out.println("Hello world, but with style checking and in codespaces!");
+        System.out.println("Hi Chris Dibert");
+    }
+}
